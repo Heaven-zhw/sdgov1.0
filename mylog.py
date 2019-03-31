54aa1a93d5c3b9afc80055044c1e82287d334b09
Heaven-zhw <912533733@qq.com>
2019-03-31 11:30:06 +0800

    爬URL页面的日志记录加上时间

--- mylog.py
+++ mylog.py
@@ -1,16 +1,20 @@
 #coding=utf-8
+import time
 #----------------------------日志记录---------------------
 #链接页面的URL是随时间滚动的，记录某时刻爬取错误的链接页面只是
 #为了记录爬取不成功的数量，不会对其进行再次爬取，因此允许重复，不必对内容去重
 #爬取URL的界面如果加载错误，记录
 def logUrlConnectError(viewHref):
+    #URL页面由于随时间滚动，日志也要记录一下时间
+    nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     with open('error_url_connect.txt', 'a+') as f:
-        f.write(str(viewHref))
+        f.write(str(viewHref)+" "+str(nowtime))
         f.write('\n')
 #爬取URL的界面如果内容格式错误，记录
 def logUrlFormError(viewHref):
+    nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     with open('error_url_form.txt', 'a+') as f:
-        f.write(str(viewHref))
+        f.write(str(viewHref) + " " + str(nowtime))
         f.write('\n')
 
 #对信息内容页面的日志内容，需要去重处理
