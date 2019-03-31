e398d736b0a4b22410caa15507a0ad45fe52d2ac
Heaven-zhw <912533733@qq.com>
2019-03-31 11:26:18 +0800

    加上导入不运行

--- dateop.py
+++ dateop.py
@@ -7,21 +7,23 @@ def dateDelta(date1,date2):
     d1=datetime.datetime.strptime(date1,'%Y%m%d')
     delta=d2-d1
     return delta.days
-s="down1.do"
-href="/sdxxgk/publi/message/down.do?id=039539"
-print(href.find(s))
-date1="2001-01-02"
-date2="2000-01-06"
-print(int(1e5))
-try:
-    ilist = [3, 5]
-    ilist.insert(0,1)
-    s1=str(ilist)
-    print(s1)
-    a=eval(s1)
-    if date1>date2:
-        #a=5/0
-        raise Exception("error")
 
-except:
-    traceback.print_exc()
+if __name__=='main':
+    s="down1.do"
+    href="/sdxxgk/publi/message/down.do?id=039539"
+    print(href.find(s))
+    date1="2001-01-02"
+    date2="2000-01-06"
+    print(int(1e5))
+    try:
+        ilist = [3, 5]
+        ilist.insert(0,1)
+        s1=str(ilist)
+        print(s1)
+        a=eval(s1)
+        if date1>date2:
+            #a=5/0
+            raise Exception("error")
+
+    except:
+        traceback.print_exc()
